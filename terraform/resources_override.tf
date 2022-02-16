45acc13f56b0542e7d0ccccb329c668ed40ed465
galargh <piotr.galar@gmail.com>
2022-02-16 16:40:12 +0100

    manage collaborators and teams only

--- terraform/resources_override.tf
+++ terraform/resources_override.tf
@@ -1,69 +1,45 @@
-resource "github_repository" "this" {
+resource "github_membership" "this" {
   lifecycle {
-    # @resources.repository.ignore_changes
+    # @resources.membership.ignore_changes
     ignore_changes = [
-      allow_auto_merge,
-      # allow_merge_commit,
-      # allow_rebase_merge,
-      # allow_squash_merge,
-      archive_on_destroy,
-      archived,
-      auto_init,
-      branches,
-      default_branch,
-      delete_branch_on_merge,
-      description,
       etag,
-      full_name,
-      git_clone_url,
-      gitignore_template,
-      has_downloads,
-      has_issues,
-      has_projects,
-      has_wiki,
-      homepage_url,
-      html_url,
-      http_clone_url,
       id,
-      is_template,
-      license_template,
-      node_id,
-      pages,
-      pages[0].cname,
-      pages[0].source[0].path,
-      private,
-      repo_id,
-      ssh_clone_url,
-      svn_url,
-      template,
-      topics,
-      visibility,
-      vulnerability_alerts
+      # role
     ]
   }
 }
 
-resource "github_branch_protection" "this" {
+resource "github_repository_collaborator" "this" {
   lifecycle {
-    # @resources.branch_protection.ignore_changes
+    # @resources.repository_collaborator.ignore_changes
     ignore_changes = [
-      allows_deletions,
-      allows_force_pushes,
-      enforce_admins,
       id,
-      push_restrictions,
-      require_conversation_resolution,
-      require_signed_commits,
-      required_linear_history,
-      # required_pull_request_reviews,
-      required_pull_request_reviews[0].dismiss_stale_reviews,
-      required_pull_request_reviews[0].dismissal_restrictions,
-      required_pull_request_reviews[0].require_code_owner_reviews,
-      required_pull_request_reviews[0].required_approving_review_count,
-      required_pull_request_reviews[0].restrict_dismissals,
-      # required_status_checks,
-      # required_status_checks[0].contexts,
-      # required_status_checks[0].strict
+      invitation_id,
+      # permission,
+      permission_diff_suppression
+    ]
+  }
+}
+
+
+resource "github_team_repository" "this" {
+  lifecycle {
+    # @resources.team_repository.ignore_changes
+    ignore_changes = [
+      etag,
+      id,
+      # permission
+    ]
+  }
+}
+
+resource "github_team_membership" "this" {
+  lifecycle {
+    # @resources.team_membership.ignore_changes
+    ignore_changes = [
+      etag,
+      id,
+      # role
     ]
   }
 }
