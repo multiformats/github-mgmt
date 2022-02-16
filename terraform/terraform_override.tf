73a4a2624881c0241cee4e8144471f570f8f4cbe
Piotr Galar <piotr.galar@gmail.com>
2022-02-16 15:23:57 +0100

    temporarily use galargh-github-mgmt bucket/table

--- terraform/terraform_override.tf
+++ terraform/terraform_override.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     region               = "us-west-2"
-    bucket               = "github-mgmt"
+    bucket               = "galargh-github-mgmt"
     key                  = "terraform.tfstate"
     workspace_key_prefix = "org"
-    dynamodb_table       = "github-mgmt"
+    dynamodb_table       = "galargh-github-mgmt"
   }
 }
